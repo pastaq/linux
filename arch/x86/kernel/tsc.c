7e15a5c14b53732419fe8c17ab9c970af9ac40bd
Steven Noonan <steven@uplinklabs.net>
2021-11-17 00:25:26 -0800

    x86: implement tsc=directsync for systems without IA32_TSC_ADJUST
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>
    [fwd-port to v6.5]
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
    Signed-off-by: Robert Beckett <bob.beckett@collabora.com>

--- arch/x86/kernel/tsc.c
+++ arch/x86/kernel/tsc.c
@@ -51,6 +51,7 @@ static unsigned int __initdata tsc_early_khz;
 static DEFINE_STATIC_KEY_FALSE_RO(__use_tsc);
 
 int tsc_clocksource_reliable;
+int tsc_allow_direct_sync;
 
 static int __read_mostly tsc_force_recalibrate;
 
@@ -344,6 +345,8 @@ static int __init tsc_setup(char *str)
 		else
 			tsc_as_watchdog = 1;
 	}
+	if (!strcmp(str, "directsync"))
+		tsc_allow_direct_sync = 1;
 	return 1;
 }
 
