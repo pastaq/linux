361ec299354186203663994758c0be1539ba663c
Christian Marcheselli <christianm@valvesoftware.com>
2023-03-31 15:21:25 -0700

    [QUECTEL/VALVE] Added in Quectel patches for bluetooth
    [Fwd-ported to v6.11.6]
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -8092,7 +8092,7 @@ static int set_external_config(struct sock *sk, struct hci_dev *hdev,
 			hci_dev_set_flag(hdev, HCI_CONFIG);
 			hci_dev_set_flag(hdev, HCI_AUTO_OFF);
 
-			queue_work(hdev->req_workqueue, &hdev->power_on);
+			queue_work(hdev->req_workqueue, &hdev->power_on.work);
 		} else {
 			set_bit(HCI_RAW, &hdev->flags);
 			mgmt_index_added(hdev);
@@ -8148,7 +8148,7 @@ static int set_public_address(struct sock *sk, struct hci_dev *hdev,
 		hci_dev_set_flag(hdev, HCI_CONFIG);
 		hci_dev_set_flag(hdev, HCI_AUTO_OFF);
 
-		queue_work(hdev->req_workqueue, &hdev->power_on);
+		queue_work(hdev->req_workqueue, &hdev->power_on.work);
 	}
 
 unlock:
