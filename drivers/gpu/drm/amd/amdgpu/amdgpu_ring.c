8fcad0371837b72e12260dba1cca8c5d72b10407
Joshua Ashton <joshua@froggi.es>
2024-01-16 00:51:25 +0000

    drm/amdgpu: Determine soft recovery deadline next to usage
    Otherwise we are determining this timeout based on a time before we go into some spinlock, which is bad.
    
    Signed-off-by: Joshua Ashton <joshua@froggi.es>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ring.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ring.c
@@ -460,8 +460,6 @@ bool amdgpu_ring_soft_recovery(struct amdgpu_ring *ring, unsigned int vmid,
 	ktime_t deadline;
 	bool ret;
 
-	deadline = ktime_add_us(ktime_get(), 10000);
-
 	if (amdgpu_sriov_vf(ring->adev) || !ring->funcs->soft_recovery || !fence)
 		return false;
 
@@ -470,6 +468,7 @@ bool amdgpu_ring_soft_recovery(struct amdgpu_ring *ring, unsigned int vmid,
 		dma_fence_set_error(fence, -ENODATA);
 	spin_unlock_irqrestore(fence->lock, flags);
 
+	deadline = ktime_add_us(ktime_get(), 10000);
 	while (!dma_fence_is_signaled(fence) &&
 	       ktime_to_ns(ktime_sub(deadline, ktime_get())) > 0)
 		ring->funcs->soft_recovery(ring, vmid);
