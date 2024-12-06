889b93961795ec723b9421f96a4ef638bd1790e1
Mario Limonciello <mario.limonciello@amd.com>
2024-12-05 21:19:00 -0600

    ACPI: platform_profile: Add platform handler argument to platform_profile_remove()
    To allow registering and unregistering multiple platform handlers calls
    to platform_profile_remove() will need to know which handler is to be
    removed.  Add an argument for this.
    
    Tested-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Tested-by: Matthew Schwartz <matthew.schwartz@linux.dev>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Reviewed-by: Maximilian Luz <luzmaximilian@gmail.com>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Reviewed-by: Armin Wolf <W_Armin@gmx.de>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/platform/x86/asus-wmi.c
+++ drivers/platform/x86/asus-wmi.c
@@ -4845,7 +4845,7 @@ fail_sysfs:
 fail_custom_fan_curve:
 fail_platform_profile_setup:
 	if (asus->platform_profile_support)
-		platform_profile_remove();
+		platform_profile_remove(&asus->platform_profile_handler);
 fail_fan_boost_mode:
 fail_platform:
 	kfree(asus);
@@ -4872,7 +4872,7 @@ static void asus_wmi_remove(struct platform_device *device)
 	asus_wmi_battery_exit(asus);
 
 	if (asus->platform_profile_support)
-		platform_profile_remove();
+		platform_profile_remove(&asus->platform_profile_handler);
 
 	kfree(asus);
 }
