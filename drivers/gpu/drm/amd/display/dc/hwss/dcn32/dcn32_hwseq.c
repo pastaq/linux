57585e3cf005e3c26d879fad12b78cacd8c0bfda
Melissa Wen <mwen@igalia.com>
2025-07-29 20:44:34 -0100

    [FOR-UPSTREAM] drm/amd/display: use dcn10 helper to translate in_shaper_lut on dcn32
    it's the same used by dcn20 and dcn30 for plane shaper LUT and have some
    differences. cm_helper doesn't support HLG TF, but we are not using it
    too.
    
    Signed-off-by: Melissa Wen <mwen@igalia.com>

--- drivers/gpu/drm/amd/display/dc/hwss/dcn32/dcn32_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/hwss/dcn32/dcn32_hwseq.c
@@ -501,10 +501,9 @@ bool dcn32_set_mcm_luts(
 	if (plane_state->in_shaper_func.type == TF_TYPE_HWPWL)
 		lut_params = &plane_state->in_shaper_func.pwl;
 	else if (plane_state->in_shaper_func.type == TF_TYPE_DISTRIBUTED_POINTS) {
-		// TODO: dpp_base replace
-		cm3_helper_translate_curve_to_hw_format(plane_state->ctx,
-							&plane_state->in_shaper_func,
-							&dpp_base->shaper_params, true);
+		cm_helper_translate_curve_to_hw_format(plane_state->ctx,
+						       &plane_state->in_shaper_func,
+						       &dpp_base->shaper_params, true);
 		lut_params = &dpp_base->shaper_params;
 	}
 
