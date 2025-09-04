b141bd542fdcbcc4754155a1bf7eba294f230ef0
Antheas Kapenekakis <lkml@antheas.dev>
2025-09-04 19:50:20 +0200

    [FROM-ML] drm: panel-orientation-quirks: Add OneXPlayer X1 Mini variants
    The OneXPlayer X1 mini features a 2k 8.8 display with a portrait
    orientation. The Pro is a CPU refresh. Add quirks to set the panel
    orientation to portrait mode. There is no Intel variant.
    
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>
    Reviewed-by: Philip Müller <philm@manjaro.org>
    Signed-off-by: Derek J. Clark <derekjohn.clark@gmail.com>
    Link: https://lore.kernel.org/r/20250904175025.3249650-6-lkml@antheas.dev

--- drivers/gpu/drm/drm_panel_orientation_quirks.c
+++ drivers/gpu/drm/drm_panel_orientation_quirks.c
@@ -516,6 +516,18 @@ static const struct dmi_system_id orientation_data[] = {
 		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "ONEXPLAYER X1 i"),
 		},
 		.driver_data = (void *)&lcd1600x2560_leftside_up,
+	}, {	/* OneXPlayer X1 mini (AMD) */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ONE-NETBOOK"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "ONEXPLAYER X1 mini"),
+		},
+		.driver_data = (void *)&lcd1600x2560_leftside_up,
+	}, {	/* OneXPlayer X1 mini pro (AMD Strix Point) */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ONE-NETBOOK"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "ONEXPLAYER X1Mini Pro"),
+		},
+		.driver_data = (void *)&lcd1600x2560_leftside_up,
 	}, {	/* OneXPlayer X1 AMD Strix Point */
 		.matches = {
 		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ONE-NETBOOK"),
