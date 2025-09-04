e26fccd7c7627095820de18db16a2751f1c799e1
Antheas Kapenekakis <lkml@antheas.dev>
2025-09-04 19:50:22 +0200

    [FROM-ML] drm: panel-orientation-quirks: Add OneXPlayer G1 variants
    Add quirks for the new clamshell device OneXPlayer G1 for both AMD
    and Intel. The device has a 1600x2560p 144hz LCD panel.
    
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>
    Reviewed-by: Philip Müller <philm@manjaro.org>
    Signed-off-by: Derek J. Clark <derekjohn.clark@gmail.com>
    Link: https://lore.kernel.org/r/20250904175025.3249650-8-lkml@antheas.dev

--- drivers/gpu/drm/drm_panel_orientation_quirks.c
+++ drivers/gpu/drm/drm_panel_orientation_quirks.c
@@ -558,6 +558,18 @@ static const struct dmi_system_id orientation_data[] = {
 		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "ONEXPLAYER F1Pro"),
 		},
 		.driver_data = (void *)&lcd1080x1920_leftside_up,
+	}, {	/* OneXPlayer OneXFly G1 AMD */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ONE-NETBOOK"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "ONEXPLAYER G1 A"),
+		},
+		.driver_data = (void *)&lcd1600x2560_leftside_up,
+	}, {	/* OneXPlayer OneXFly G1 Intel */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ONE-NETBOOK"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "ONEXPLAYER G1 i"),
+		},
+		.driver_data = (void *)&lcd1600x2560_leftside_up,
 	}, {	/* Zotac Gaming Zone (OLED) */
 		.matches = {
 		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ZOTAC"),
