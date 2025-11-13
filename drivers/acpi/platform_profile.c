07d9de3a1f7204de03c8b916c87d4e6c7fa58a65
Derek J. Clark <derekjohn.clark@gmail.com>
2025-11-13 13:26:15 -0800

    [FROM-ML] acpi: platform_profile - Add max-power profile option
    Some devices, namely Lenovo Legion devices, have an "extreme" mode where
    power draw is at the maximum limit of the cooling hardware. Add a new
    "max-power" platform profile to properly reflect this operating mode.
    
    Reviewed-by: Mario Limonciello (AMD) <superm1@kernel.org>
    Acked-by: Rafael J. Wysocki (Intel) <rafael@kernel.org>
    Signed-off-by: Derek J. Clark <derekjohn.clark@gmail.com>
    Reviewed-by: Armin Wolf <W_Armin@gmx.de>
    Link: https://lore.kernel.org/r/20251113212639.459896-2-derekjohn.clark@gmail.com
    Signed-off-by: Robert Beckett <bob.beckett@collabora.com>

--- drivers/acpi/platform_profile.c
+++ drivers/acpi/platform_profile.c
@@ -37,6 +37,7 @@ static const char * const profile_names[] = {
 	[PLATFORM_PROFILE_BALANCED] = "balanced",
 	[PLATFORM_PROFILE_BALANCED_PERFORMANCE] = "balanced-performance",
 	[PLATFORM_PROFILE_PERFORMANCE] = "performance",
+	[PLATFORM_PROFILE_MAX_POWER] = "max-power",
 	[PLATFORM_PROFILE_CUSTOM] = "custom",
 };
 static_assert(ARRAY_SIZE(profile_names) == PLATFORM_PROFILE_LAST);
