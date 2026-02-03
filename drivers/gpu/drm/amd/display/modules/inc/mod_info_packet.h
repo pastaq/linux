dc3a5f5157d7a57b7e872c4f9a86881f924f2d35
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-02-03 19:56:18 +0100

    [FOR-UPSTREAM] drm/amd/display: Support HDMI VRRmax=0
    [Why]
    VRRmax=0 is a valid value and means that the upper bound is guared by
    the selected video mode.
    
    [How]
    In this context, saved vrr max is the max possible refresh rate ever.
    Try getting upper VRR bound from AMD vsdbif it exists or rely on the
    limitations of BRR in VTEM info frames.
    
    I found through testing, that TVs seem to reject VTEM when BRR is set
    to over 1023 Hz. Use this as the last resort VRRmax. 1023 is the max
    value for a 10-bit field as well.
    
    v2:
    - Update max BRR value to 1023
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>

--- drivers/gpu/drm/amd/display/modules/inc/mod_info_packet.h
+++ drivers/gpu/drm/amd/display/modules/inc/mod_info_packet.h
@@ -33,6 +33,8 @@ struct dc_stream_state;
 struct dc_info_packet;
 struct mod_vrr_params;
 
+#define VTEM_BRR_MAX 1023
+
 void mod_build_vsc_infopacket(const struct dc_stream_state *stream,
 		struct dc_info_packet *info_packet,
 		enum dc_color_space cs,
