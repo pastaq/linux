e920cde5b6758d15634b34aaa13f353af1ea4024
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-02-03 19:56:22 +0100

    [FOR-UPSTREAM] drm/amd/display: Trigger ALLM if it's available
    [Why]
    ALLM automatically puts TVs into low latency modes (gaming modes) which
    we basically always want for PC use, be it gaming, or using precise
    inputs like mice and keyboards.
    
    [How]
    Read the ALLM info from HDMI caps and use it to determine if ALLM should
    be indicated in HDMI Forum vsif. Additionally, make sure VIC modes are
    translated in case of ALLM active as VIC cannot be used in conjunction
    with hf-vsif. I learned this the hard way...
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_resource.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_resource.c
@@ -4473,6 +4473,7 @@ static void set_avi_info_frame(
 	unsigned int rid = pipe_ctx->stream->timing.rid;
 	unsigned int fr_ind = pipe_ctx->stream->timing.fr_index;
 	enum dc_timing_3d_format format;
+	bool allm;
 
 	if (stream->avi_infopacket.valid) {
 		*info_packet = stream->avi_infopacket;
@@ -4627,8 +4628,9 @@ static void set_avi_info_frame(
 	if (pipe_ctx->stream->timing.hdmi_vic != 0)
 		vic = 0;
 	format = stream->timing.timing_3d_format;
+	allm = stream->link->local_sink->edid_caps.allm;
 	/*todo, add 3DStereo support*/
-	if (format != TIMING_3D_FORMAT_NONE) {
+	if ((format != TIMING_3D_FORMAT_NONE) || allm) {
 		// Based on HDMI specs hdmi vic needs to be converted to cea vic when 3D is enabled
 		switch (pipe_ctx->stream->timing.hdmi_vic) {
 		case 1:
