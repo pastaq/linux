546ce16052e5c8c0e0a453106af5daa98527d6d3
Derek J. Clark <derekjohn.clark@gmail.com>
2026-05-22 03:15:29 +0000

    [NOT-FOR-UPSTREAM] HID: hid-msi: Add name to struct member for reports.
    Kernel 6.19 enabled fms extensions, which allows structs to have anonymous
    structs as members, providing direct access to those child members. Instead
    of backporting that patch, add a name to the struct member here and
    access them through it.
    
    See commit c4781dc3d1cf ("Kbuild: enable -fms-extensions")
    
    Singed-off-by: Derek J. Clark <derekojhn.clark@gmail.com>
    Signed-off-by: Nícolas F. R. A. Prado <nfraprado@collabora.com>

--- drivers/hid/hid-msi.c
+++ drivers/hid/hid-msi.c
@@ -287,7 +287,7 @@ struct claw_profile_report {
 } __packed;
 
 struct claw_mkey_report {
-	struct claw_profile_report;
+	struct claw_profile_report p;
 	u8 padding_0;
 	u8 padding_1;
 	u8 padding_2;
@@ -305,7 +305,7 @@ struct rgb_frame {
 };
 
 struct claw_rgb_report {
-	struct claw_profile_report;
+	struct claw_profile_report p;
 	u8 frame_bytes;
 	u8 padding;
 	u8 frame_count;
@@ -316,7 +316,7 @@ struct claw_rgb_report {
 } __packed;
 
 struct claw_rumble_report {
-	struct claw_profile_report;
+	struct claw_profile_report p;
 	u8 padding;
 	u8 intensity;
 } __packed;
@@ -415,7 +415,7 @@ static int claw_profile_event(struct claw_drvdata *drvdata, struct claw_command_
 	case CLAW_M2_PENDING:
 		key = (profile == CLAW_M1_PENDING) ? CLAW_KEY_M1 : CLAW_KEY_M2;
 		mkeys = (struct claw_mkey_report *)cmd_rep->data;
-		if (be16_to_cpu(mkeys->read_addr) != drvdata->bmap_addr[key])
+		if (be16_to_cpu(mkeys->p.read_addr) != drvdata->bmap_addr[key])
 			return -EINVAL;
 		codes = (profile == CLAW_M1_PENDING) ? drvdata->m1_codes : drvdata->m2_codes;
 		for (i = 0; i < CLAW_KEYS_MAX; i++)
@@ -424,7 +424,7 @@ static int claw_profile_event(struct claw_drvdata *drvdata, struct claw_command_
 	case CLAW_RGB_PENDING:
 		frame = (struct claw_rgb_report *)cmd_rep->data;
 		rgb_addr = drvdata->rgb_addr;
-		read_addr = be16_to_cpu(frame->read_addr);
+		read_addr = be16_to_cpu(frame->p.read_addr);
 		frame_calc = (read_addr - rgb_addr) / CLAW_RGB_FRAME_OFFSET;
 		if (frame_calc >= CLAW_RGB_MAX_FRAMES) {
 			dev_err(&drvdata->hdev->dev, "Got unsupported frame index: %x\n",
@@ -451,14 +451,14 @@ static int claw_profile_event(struct claw_drvdata *drvdata, struct claw_command_
 		break;
 	case CLAW_RUMBLE_LEFT_PENDING:
 		rumble = (struct claw_rumble_report *)cmd_rep->data;
-		if (be16_to_cpu(rumble->read_addr) != rumble_addr[0])
+		if (be16_to_cpu(rumble->p.read_addr) != rumble_addr[0])
 			return -EINVAL;
 		scoped_guard(spinlock, &drvdata->rumble_lock)
 			drvdata->rumble_intensity_left = rumble->intensity;
 		break;
 	case CLAW_RUMBLE_RIGHT_PENDING:
 		rumble = (struct claw_rumble_report *)cmd_rep->data;
-		if (be16_to_cpu(rumble->read_addr) != rumble_addr[1])
+		if (be16_to_cpu(rumble->p.read_addr) != rumble_addr[1])
 			return -EINVAL;
 		scoped_guard(spinlock, &drvdata->rumble_lock)
 			drvdata->rumble_intensity_right = rumble->intensity;
@@ -1219,7 +1219,7 @@ static int claw_write_rgb_state(struct claw_drvdata *drvdata)
 			report.zone_data = drvdata->rgb_frames[f];
 
 		/* Set the MCU address to write the frame data to */
-		report.read_addr = cpu_to_be16(write_addr);
+		report.p.read_addr = cpu_to_be16(write_addr);
 
 		/* Serialize the rgb_report and write it to MCU */
 		ret = claw_hw_output_report(drvdata->hdev, CLAW_COMMAND_TYPE_WRITE_PROFILE_DATA,
