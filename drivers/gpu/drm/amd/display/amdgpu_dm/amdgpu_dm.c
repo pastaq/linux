f66ec3746f03e8f87e3b8ef7dff17aa9b5fd09a6
Natalie Vock <natalie.vock@gmx.de>
2026-06-13 16:29:37 +0200

    [FOR-UPSTREAM] drm/amd/display: Wait for flip completion before cleaning up fbs
    When performing async flips, we can dispatch the flip without waiting
    for vblank, but we still need to actually wait for the flip. Otherwise
    the framebuffer objects are unpinned too early and things to way south
    if we happen to be under memory pressure and try evicting the
    framebuffers.
    
    Signed-off-by: Natalie Vock <natalie.vock@gmx.de>
    Signed-off-by: Nícolas F. R. A. Prado <nfraprado@collabora.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -11141,9 +11141,7 @@ static void amdgpu_dm_atomic_commit_tail(struct drm_atomic_state *state)
 	/* Signal HW programming completion */
 	drm_atomic_helper_commit_hw_done(state);
 
-	if (wait_for_vblank)
-		drm_atomic_helper_wait_for_flip_done(dev, state);
-
+	drm_atomic_helper_wait_for_flip_done(dev, state);
 	drm_atomic_helper_cleanup_planes(dev, state);
 
 	/* Don't free the memory if we are hitting this as part of suspend.
