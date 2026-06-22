0a72160e084107eb360b9eafa1124868bbedccb5
Matthew Schwartz <matthew.schwartz@linux.dev>
2026-06-22 11:43:26 -0700

    [FOR-UPSTREAM] mmc: rtsx: add MSI Claw A8 to quirk table for NO_AGGRESSIVE_PM
    Signed-off-by: Matthew Schwartz <matthew.schwartz@linux.dev>

--- drivers/mmc/host/rtsx_pci_sdmmc.c
+++ drivers/mmc/host/rtsx_pci_sdmmc.c
@@ -120,6 +120,14 @@ static const struct dmi_system_id rtsx_pci_sdmmc_dmi_quirks[] = {
 			DMI_MATCH(DMI_BOARD_NAME, "MS-1T52"),
 		},
 	},
+	{
+		/* MSI Claw A8 BZ2EM */
+		.driver_data = (void *)QUIRK_NO_AGGRESSIVE_PM,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "Micro-Star International Co., Ltd."),
+			DMI_MATCH(DMI_BOARD_NAME, "MS-1T8K"),
+		},
+	},
 	{},
 };
 
