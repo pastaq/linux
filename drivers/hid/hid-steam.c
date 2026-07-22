102a761da611cd138b818d0fd71c8bce93671b7c
Nícolas F. R. A. Prado <nfraprado@collabora.com>
2026-07-22 15:29:00 -0300

    Revert "[FOR-UPSTREAM] HID: steam: Fix wording of connect/disconnect logs"
    This reverts commit 84f6d54281ea577b4c4a3f7de45247bef5e72794.

--- drivers/hid/hid-steam.c
+++ drivers/hid/hid-steam.c
@@ -1383,8 +1383,7 @@ static int steam_register(struct steam_device *steam)
 			"%s:steam_get_attributes failed with error %d\n",
 			__func__, ret);
 
-	hid_info(steam->hdev, "Steam %s '%s' connected",
-			steam->quirks & STEAM_QUIRK_DECK ? "Deck" : "Controller",
+	hid_info(steam->hdev, "Steam Controller '%s' connected",
 			steam->serial_no);
 
 	/* ignore battery errors, we can live without it */
@@ -1416,8 +1415,7 @@ static void steam_unregister(struct steam_device *steam)
 	if (!steam->serial_no[0])
 		return;
 
-	hid_info(steam->hdev, "Steam %s '%s' disconnected",
-			steam->quirks & STEAM_QUIRK_DECK ? "Deck" : "Controller",
+	hid_info(steam->hdev, "Steam Controller '%s' disconnected",
 			steam->serial_no);
 	steam_battery_unregister(steam);
 	steam_sensors_unregister(steam);
